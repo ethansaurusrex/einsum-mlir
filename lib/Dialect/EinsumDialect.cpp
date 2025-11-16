34fe7edd6d7021e0f77f694848f8b1e459ad4ab9
Ethan Matus <ethanfmatus@gmail.com>
2025-11-16 00:57:01 -0500

    Added outline for dialect

---
+++ lib/Dialect/EinsumDialect.cpp
@@ -0,0 +1,11 @@
+#include "lib/Dialect/EinsumDialect.h"
+
+#include "lib/Dialect/EinsumDialect.cpp.inc"
+
+namespace mlir {
+
+  void EinsumDialect::initialize() {
+    
+  };
+
+} // namespace mlir
