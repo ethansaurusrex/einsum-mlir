896e4b3fcb95734d1a94d1c5a2aef5778a3e20de
Ethan Matus <ethanfmatus@gmail.com>
2025-11-16 00:57:48 -0500

    Added tools/

---
+++ tools/einsum-opt/einsum-opt.cpp
@@ -0,0 +1,15 @@
+#include "lib/Dialect/EinsumDialect.h"
+#include "mlir/IR/MLIRContext.h"
+#include "mlir/InitAllDialects.h"
+#include "mlir/Pass/PassManager.h"
+#include "mlir/Pass/PassRegistry.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+
+int main(int argc, char **argv) {
+  mlir::DialectRegistry registry;
+  registry.insert<mlir::EinsumDialect>();
+  mlir::registerAllDialects(registry);
+  
+  return mlir::asMainReturnCode(
+				mlir::MlirOptMain(argc, argv, "Einsum MLIR optimizer\n", registry));
+}
