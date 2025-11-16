ecda829ae6d2a0a606af55d92092a3adc99b659f
Ethan Matus <ethanfmatus@gmail.com>
2025-11-16 12:03:18 -0500

    updated Einsum types

--- lib/Dialect/Einsum/EinsumDialect.cpp
+++ lib/Dialect/Einsum/EinsumDialect.cpp
@@ -1,11 +1,20 @@
 #include "lib/Dialect/Einsum/EinsumDialect.h"
 
+#include "lib/Dialect/Einsum/EinsumTypes.h"
+#include "mlir/IR/Builders.h"
+#include "llvm/ADT/TypeSwitch.h"
+
 #include "lib/Dialect/Einsum/EinsumDialect.cpp.inc"
+#define GET_TYPEDEF_CLASSES
+#include "lib/Dialect/Einsum/EinsumTypes.cpp.inc"
 
 namespace mlir {
   namespace einsum {
     void EinsumDialect::initialize() {
-    
+    addTypes<
+      #define GET_TYPEDEF_LIST
+      #include "lib/Dialect/Einsum/EinsumTypes.cpp.inc"
+      >();
     };
   } // einsum
 } // namespace mlir
---
+++ lib/Dialect/Einsum/EinsumTypes.h
@@ -0,0 +1,9 @@
+#ifndef LIB_DIALECT_EINSUM_EINSUMTYPES_H_
+#define LIB_DIALECT_EINSUM_EINSUMTYPES_H_
+
+#include "mlir/IR/DialectImplementation.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "lib/Dialect/Einsum/EinsumTypes.h.inc"
+
+#endif // LIB_DIALECT_EINSUM_EINSUMTYPES_H_
