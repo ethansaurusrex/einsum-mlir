f71d8b50acd7952cfe388e14d1a52d18b29a6ad5
Ethan Matus <ethanfmatus@gmail.com>
2025-11-20 02:07:31 -0500

    Einsum Dialect CAPI now compiling correctly

--- python/EinsumModule.cpp
+++ python/EinsumModule.cpp
@@ -1,6 +1,8 @@
+
 #include "mlir-c/IR.h"
 #include "mlir/Bindings/Python/PybindAdaptors.h"
 #include "lib/Dialect/Einsum/EinsumDialect.h"
+#include "lib/Dialect/Einsum/CAPI/Dialect.h"
 #include "lib/Transforms/Einsum/Passes.h"
 
 namespace py = pybind11;
@@ -10,20 +12,30 @@ using namespace mlir::einsum;
 void populateEinsumDialect(py::module &m) {
   m.def("register_dialect", [](MlirContext context, bool load) {
     MlirDialectHandle handle = mlirGetDialectHandle__einsum__();
+    mlirDialectHandleRegisterDialect(handle, context);
+    if (load) {
+      mlirDialectHandleLoadDialect(handle, context);
+    }
+  }, py::arg("context"), py::arg("load") = true);
+}
+/*
     mlir::MLIRContext *cppCtx = unwrap(context);
     cppCtx->getOrLoadDialect<EinsumDialect>();
   });
 }
+*/
 
+/*
 // pass registration hook
 void populateEinsumPasses(py::module &m) {
   m.def("register_passes", []() {
     registerEinsumPasses();
   });
 }
+*/
 
 PYBIND11_MODULE(_einsum, m) {
   m.doc() = "Einsum MLIR Dialect Python Bindings";
   populateEinsumDialect(m);
-  populateEinsumPasses(m);
+  //populateEinsumPasses(m);
 }
